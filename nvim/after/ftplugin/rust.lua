eee19ad0851409a9e94494447d4cfbea9609e7ae
Erik Teichmann <erik.teichmann@ambrosys.de>
2024-01-16 20:16:42 +0100

    Remove Python lsp

--- nvim/after/ftplugin/rust.lua
+++ nvim/after/ftplugin/rust.lua
@@ -1,7 +1,7 @@
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
-vim.opt.tabstop = 3
+vim.opt.tabstop = 4
 vim.opt.colorcolumn = "100"
 
 vim.api.nvim_create_autocmd("BufWritePre", {
--- nvim/lua/config/mason_lspconfig.lua
+++ nvim/lua/config/mason_lspconfig.lua
@@ -2,7 +2,7 @@ return {
    "williamboman/mason-lspconfig.nvim",
    dependencies = {"neovim/nvim-lspconfig"},
    opts = {
-      ensure_installed = {"lua_ls", "rust_analyzer", "ruff_lsp", "pyright", "pylsp"}
+      ensure_installed = {"lua_ls", "rust_analyzer"}
    },
    config = true
 }
