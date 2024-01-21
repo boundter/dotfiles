ec586404b16c3390ffa33f02a750ef8a48d04b0a
Erik Teichmann <kontakt.teichmann@gmail.com>
2024-01-21 08:31:11 +0100

    Cleanup

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -18,31 +18,6 @@ return {
    { "williamboman/mason.nvim", lazy = false, config = true },
    require("config.mason_lspconfig"),
    require("config.cmp"),
-   {
-      'mrcjkb/rustaceanvim',
-      version = '^3', -- Recommended
-      ft = { 'rust' },
-   },
-   {"scalameta/nvim-metals",
-   ft = { "scala", "sbt", "java" },
-   opts = function()
-      local metals_config = require("metals").bare_config()
-      metals_config.settings = {
-         showImplicitArguments = true,
-      }
-      metals_config.init_options.statusBarProvider = "on"
-      metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
-      return metals_config
-   end,
-   config = function(self, metals_config)
-      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-      vim.api.nvim_create_autocmd("FileType", {
-         pattern = self.ft,
-         callback = function()
-            require("metals").initialize_or_attach(metals_config)
-         end,
-         group = nvim_metals_group,
-      })
-   end
-   },
+   require("config.rustaceanvim"),
+   require("config.nvim_metals"),
 }
