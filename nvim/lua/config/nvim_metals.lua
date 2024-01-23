a4fffa39b22c3d8bb3f23b561c55fc9243348dad
Erik Teichmann <erik.teichmann.extern@toll-collect.de>
2024-01-23 08:26:09 +0100

    Small Improvements

--- nvim/lua/config/nvim_metals.lua
+++ nvim/lua/config/nvim_metals.lua
@@ -6,7 +6,7 @@ return {
       metals_config.settings = {
          showImplicitArguments = true,
       }
-      metals_config.init_options.statusBarProvider = "on"
+      -- metals_config.init_options.statusBarProvider = "on"
       metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
       return metals_config
    end,
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,4 +1,5 @@
 vim.opt.number = true  -- line numbers
+vim.opt.relativenumber = true  -- relative linenumbers
 vim.opt.expandtab = true  -- tabs to spaces
 vim.opt.showmatch = true  -- matching braces
 vim.opt.hlsearch = true
