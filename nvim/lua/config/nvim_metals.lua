587dbbf27dc97b7f8d2269b7d208d0080f1815aa
Erik Teichmann <erik.teichmann.extern@toll-collect.de>
2024-01-27 17:08:33 +0100

    Enable Project Switching in tmux

--- nvim/lua/config/nvim_metals.lua
+++ nvim/lua/config/nvim_metals.lua
@@ -5,6 +5,7 @@ return {
       local metals_config = require("metals").bare_config()
       metals_config.settings = {
          showImplicitArguments = true,
+         serverProperties = {"-Xmx4G"},
       }
       -- metals_config.init_options.statusBarProvider = "on"
       metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
