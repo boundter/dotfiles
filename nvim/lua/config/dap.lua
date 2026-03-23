8a2b00328e945db6a78a88c11a0c78de1f5db65a
Erik Teichmann <erik.teichmann.extern@toll-collect.de>
2026-03-23 08:27:35 +0100

    Fix: DAP-Configuration

--- nvim/lua/config/dap.lua
+++ nvim/lua/config/dap.lua
@@ -11,5 +11,29 @@ return {
       },
       "theHamsta/nvim-dap-virtual-text",
    },
-   keys = require("config.keys").dap
+   keys = require("config.keys").dap,
+   config = function()
+      local dap = require("dap")
+      local dap_virtual_text = require("nvim-dap-virtual-text")
+      local dapui = require("dapui")
+
+      dap_virtual_text.setup()
+      dapui.setup()
+
+      dap.listeners.before.attach.dapui_config = function()
+         dapui.open()
+      end
+
+      dap.listeners.before.launch.dapui_config = function()
+         dapui.open()
+      end
+
+      dap.listeners.before.event_terminated.dapui_config = function()
+         dapui.close()
+      end
+
+      dap.listeners.before.event_exited.dapui_config = function()
+         dapui.close()
+      end
+   end,
 }
